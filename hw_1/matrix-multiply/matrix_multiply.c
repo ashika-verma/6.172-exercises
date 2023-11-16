c5b17857acfdde5fe54afb52bc9ee66d0625bda5
Ashika <ashikav2000@gmail.com>
2023-11-15 19:57:18 -0500

    fix testbed

--- hw_1/matrix-multiply/matrix_multiply.c
+++ hw_1/matrix-multiply/matrix_multiply.c
@@ -32,28 +32,32 @@
 #include <math.h>
 #include <string.h>
 
-// #include "./tbassert.h"
+#include "./tbassert.h"
 
 // Allocates a row-by-cols matrix and returns it
-matrix* make_matrix(int rows, int cols) {
-  matrix* new_matrix = malloc(sizeof(matrix));
+matrix *make_matrix(int rows, int cols)
+{
+  matrix *new_matrix = malloc(sizeof(matrix));
 
   // Set the number of rows and columns
   new_matrix->rows = rows;
   new_matrix->cols = cols;
 
   // Allocate a buffer big enough to hold the matrix.
-  new_matrix->values = (int**)malloc(sizeof(int*) * rows);
-  for (int i = 0; i < rows; i++) {
-    new_matrix->values[i] = (int*)malloc(sizeof(int) * cols);
+  new_matrix->values = (int **)malloc(sizeof(int *) * rows);
+  for (int i = 0; i < rows; i++)
+  {
+    new_matrix->values[i] = (int *)malloc(sizeof(int) * cols);
   }
 
   return new_matrix;
 }
 
 // Frees an allocated matrix
-void free_matrix(matrix* m) {
-  for (int i = 0; i < m->rows; i++) {
+void free_matrix(matrix *m)
+{
+  for (int i = 0; i < m->rows; i++)
+  {
     free(m->values[i]);
   }
   free(m->values);
@@ -61,10 +65,13 @@ void free_matrix(matrix* m) {
 }
 
 // Print matrix
-void print_matrix(const matrix* m) {
+void print_matrix(const matrix *m)
+{
   printf("------------\n");
-  for (int i = 0; i < m->rows; i++) {
-    for (int j = 0; j < m->cols; j++) {
+  for (int i = 0; i < m->rows; i++)
+  {
+    for (int j = 0; j < m->cols; j++)
+    {
       printf("  %3d  ", m->values[i][j]);
     }
     printf("\n");
@@ -72,21 +79,24 @@ void print_matrix(const matrix* m) {
   printf("------------\n");
 }
 
-
 // Multiply matrix A*B, store result in C.
-int matrix_multiply_run(const matrix* A, const matrix* B, matrix* C) {
-  /*
+int matrix_multiply_run(const matrix *A, const matrix *B, matrix *C)
+{
+  // /*
   tbassert(A->cols == B->rows,
            "A->cols = %d, B->rows = %d\n", A->cols, B->rows);
   tbassert(A->rows == C->rows,
            "A->rows = %d, C->rows = %d\n", A->rows, C->rows);
   tbassert(B->cols == C->cols,
            "B->cols = %d, C->cols = %d\n", B->cols, C->cols);
-  */
+  // */
 
-  for (int i = 0; i < A->rows; i++) {
-    for (int j = 0; j < B->cols; j++) {
-      for (int k = 0; k < A->cols; k++) {
+  for (int i = 0; i < A->rows; i++)
+  {
+    for (int j = 0; j < B->cols; j++)
+    {
+      for (int k = 0; k < A->cols; k++)
+      {
         C->values[i][j] += A->values[i][k] * B->values[k][j];
       }
     }

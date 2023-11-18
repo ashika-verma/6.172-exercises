39e565a141729281602c19cb8060b514bff9fed2
Ashika <ashikav2000@gmail.com>
2023-11-17 23:42:32 -0500

    bitset optimization

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <chrono>
+#include <bitset>
 using namespace std;
 
 constexpr int size = 9;
@@ -49,20 +49,20 @@ vector<vector<int>> getEmptyBoxes(int puzzle[size][size])
     return empty_boxes;
 }
 
-set<int> get_valid_numbers(const int puzzle[size][size], const vector<int> &location)
+bitset<10> get_valid_numbers(const int puzzle[size][size], const vector<int> &location)
 {
     int row = location[0];
     int col = location[1];
 
-    set<int> nums_in_adj = {};
+    bitset<10> invalid_nums;
 
     // try the col
     for (int x = 0; x < size; x++)
     {
         int row_num = puzzle[x][col];
         int col_num = puzzle[row][x];
-        nums_in_adj.insert(row_num);
-        nums_in_adj.insert(col_num);
+        invalid_nums.set(row_num);
+        invalid_nums.set(col_num);
     }
 
     int box_r = row / 3;
@@ -72,24 +72,10 @@ set<int> get_valid_numbers(const int puzzle[size][size], const vector<int> &loca
     {
         for (int c = box_c * 3; c < 3 * box_c + 3; c++)
         {
-            if (puzzle[r][c] != 0)
-            {
-                nums_in_adj.insert(puzzle[r][c]);
-            }
+            invalid_nums.set(puzzle[r][c]);
         }
     }
-
-    set<int> nums_to_try = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    for (auto &x : nums_in_adj)
-    {
-        if (nums_to_try.find(x) != nums_to_try.end())
-        {
-            nums_to_try.erase(x);
-        }
-    }
-
-    return nums_to_try;
+    return invalid_nums;
 }
 
 bool solve_board(int puzzle[size][size], vector<vector<int>> locations)
@@ -104,7 +90,7 @@ bool solve_board(int puzzle[size][size], vector<vector<int>> locations)
     int r = location[0];
     int c = location[1];
 
-    set<int> valid_nums = get_valid_numbers(puzzle, location);
+    bitset<10> valid_nums = get_valid_numbers(puzzle, location);
     if (valid_nums.size() == 0)
     {
         return false;
@@ -112,14 +98,18 @@ bool solve_board(int puzzle[size][size], vector<vector<int>> locations)
 
     vector<vector<int>> rest(locations.begin() + 1, locations.end());
 
-    for (int valid_num : valid_nums)
+    for (int i = 1; i < 10; i++)
     {
-        puzzle[r][c] = valid_num;
-        if (solve_board(puzzle, rest))
+
+        if (!valid_nums[i])
         {
-            return true;
+            puzzle[r][c] = i;
+            if (solve_board(puzzle, rest))
+            {
+                return true;
+            }
+            puzzle[r][c] = 0;
         }
-        puzzle[r][c] = 0;
     }
 
     return false;
@@ -132,7 +122,6 @@ int main()
 
     auto start = chrono::high_resolution_clock::now();
     vector<vector<int>> empty_boxes = getEmptyBoxes(sudoku_board);
-    set<int> nums = get_valid_numbers(sudoku_board, empty_boxes[0]);
     solve_board(sudoku_board, empty_boxes);
     auto end = chrono::high_resolution_clock::now();
 
--- sudoku/timings.md
+++ sudoku/timings.md
@@ -1,3 +1,4 @@
 ## Sudoku
 - initial speed: ~565ms
 - with compiler flag -O3, 148ms
+- with bitset instead of set, 129ms

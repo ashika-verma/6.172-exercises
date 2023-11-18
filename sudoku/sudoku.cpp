c93e48da9240ce883ff5e222088a30aafb5eb48f
Ashika <ashikav2000@gmail.com>
2023-11-18 15:44:22 -0500

    inlining func speedup

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -33,8 +33,8 @@ void printSudokuBoard()
 constexpr int empty_size = size * size * 2;
 struct boardDesc
 {
-    int doubledSize;
-    int emptyBoxes[empty_size];
+    int doubledSize;            // tells you when we get to the end of empty boxes
+    int emptyBoxes[empty_size]; // interleaved [r_1, c_1, r_2, c_2]
 };
 
 boardDesc getEmptyBoxes(const int (&puzzle)[size][size])
@@ -57,35 +57,6 @@ boardDesc getEmptyBoxes(const int (&puzzle)[size][size])
     return board;
 }
 
-bitset<10> get_valid_numbers(const int (&puzzle)[size][size], const int (&location)[2])
-{
-    int row = location[0];
-    int col = location[1];
-
-    bitset<10> invalid_nums;
-
-    // try the col
-    for (int x = 0; x < size; x++)
-    {
-        int row_num = puzzle[x][col];
-        int col_num = puzzle[row][x];
-        invalid_nums.set(row_num);
-        invalid_nums.set(col_num);
-    }
-
-    int box_r = lookupTable[row];
-    int box_c = lookupTable[col];
-
-    for (int r = box_r; r < box_r + 3; r++)
-    {
-        for (int c = box_c; c < box_c + 3; c++)
-        {
-            invalid_nums.set(puzzle[r][c]);
-        }
-    }
-    return invalid_nums.flip();
-}
-
 bool solve_board(int (&puzzle)[size][size], boardDesc &board, const int curr_loc)
 {
 
@@ -97,12 +68,30 @@ bool solve_board(int (&puzzle)[size][size], boardDesc &board, const int curr_loc
     int r = board.emptyBoxes[curr_loc];
     int c = board.emptyBoxes[curr_loc + 1];
 
-    bitset<10>
-        valid_nums = get_valid_numbers(puzzle, {r, c});
+    bitset<10> valid_nums; // 0,000,000,000
+
+    // try the col
+    for (int x = 0; x < size; x++)
+    {
+        int row_num = puzzle[x][c];
+        int col_num = puzzle[r][x];
+        valid_nums.set(row_num).set(col_num);
+    }
+
+    int box_r = lookupTable[r];
+    int box_c = lookupTable[c];
+
+    for (int box_it_r = box_r; box_it_r < box_r + 3; box_it_r++)
+    {
+        for (int box_it_c = box_c; box_it_c < box_c + 3; box_it_c++)
+        {
+            valid_nums.set(puzzle[box_it_r][box_it_c]);
+        }
+    }
+    valid_nums.flip();
 
     for (int i = 1; i < 10; i++)
     {
-
         if (valid_nums[i])
         {
             puzzle[r][c] = i;
